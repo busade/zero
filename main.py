f1d51ff2afbcebd5cd991adf83860a80b5c493e8
Adesola Busari <adesolaisa3@gmail.com>
2025-10-16 10:37:04 +0100

    heroku

--- main.py
+++ main.py
@@ -39,21 +39,19 @@ async def shutdown_event():
 async def fetch_cat() -> str:
     """Fetch random cat fact"""
     try:
-        response = await HTTP_CLIENT.get(url, timeout=60)
-
+        response = await app.state.http_client.get(url, timeout=60)
         response.raise_for_status()
-        data= response.json()
+        data = response.json()
         return data.get("fact", "No fact available")
     except httpx.TimeoutException:
         print("ERROR: Cat API call timed out.")
         return "No fact available"
     except httpx.HTTPStatusError as e:
         print(f"ERROR: Cat API returned status code {e.response.status_code}")
-        return"No fact available"
+        return "No fact available"
     except Exception as e:
         print(f"ERROR: An unexpected error occurred: {e}")
         return "No fact available"
-    
 
 @app.get('/me',
          status_code=status.HTTP_200_OK,
